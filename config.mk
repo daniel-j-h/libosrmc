feb90e4e699e3894cf532c29db4ced6bb902df62
Daniel J. Hofmann <daniel@trvx.org>
2016-04-04 14:05:42 +0200

    Removes osrm v5 pkg-config workaround, fixed upstream
    See: https://github.com/Project-OSRM/osrm-backend/pull/2208

--- config.mk
+++ config.mk
@@ -1,3 +1,3 @@
-CXXFLAGS = -O2 -Wall -Wextra -pedantic -std=c++11 -fvisibility=hidden -fPIC -fno-rtti -I$(shell pkg-config --cflags libosrm)
+CXXFLAGS = -O2 -Wall -Wextra -pedantic -std=c++11 -fvisibility=hidden -fPIC -fno-rtti $(shell pkg-config --cflags libosrm)
 LDFLAGS  = -shared -Wl,-soname,libosrmc.so.1
 LDLIBS   = -lstdc++ $(shell pkg-config --libs libosrm)
