adad1e5e8e06ced68fd80c69085b2eda0e45bd4e
Daniel J. Hofmann <daniel@trvx.org>
2016-10-03 09:53:18 +0200

    Tested with 5.4

--- libosrmc/config.mk
+++ libosrmc/config.mk
@@ -1,7 +1,7 @@
 PREFIX = /usr/local
 
 VERSION_MAJOR = 5
-VERSION_MINOR = 3
+VERSION_MINOR = 4
 
 CXXFLAGS = -O2 -Wall -Wextra -pedantic -std=c++11 -fvisibility=hidden -fPIC -fno-rtti $(shell pkg-config --cflags libosrm)
 LDFLAGS  = -shared -Wl,-soname,libosrmc.so.$(VERSION_MAJOR)
